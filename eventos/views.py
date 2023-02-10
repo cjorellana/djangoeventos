a225388e07211b867bcbe7bc830a49f682eda9c9
CARLOS JUAN ORELLANA <cjorellana@gmail.com>
2023-02-10 13:10:05 -0600

    se agrega mejoras

--- eventos/views.py
+++ eventos/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Evento,Persona,Pais
+
 
 # Create your views here.
 
-def index(request):            
-    return render(request,'index.html')
+def index(request):
+
+    eventos = Evento.objects.all()
+
+    data = {
+        'titulo': "Eventos",
+        'listado': eventos
+    } 
+    
+    return render(request,'index.html',data)
     
 def about(request):
     return render(request,"about.html")
+
+
+    
