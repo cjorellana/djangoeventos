5ebe2738d85c0b25ad62c5e2d7be8d52de12802e
CARLOS JUAN ORELLANA <cjorellana@gmail.com>
2023-04-26 12:46:06 -0600

    se agrega vista como mensajes

--- eventos/urls.py
+++ eventos/urls.py
@@ -5,12 +5,14 @@ from . import views
 
 handler404 = 'eventos.views.custom_404'
 
-urlpatterns = [    
-    #path('', views.index,name="index"),
+urlpatterns = [
     path('', views.EventosListView.as_view(),name="index"),
-
     path("about/", TemplateView.as_view(template_name="about.html")),
+    path('detalle/<pk>', views.detalle.as_view(),name="detalle"),
+    path('contacto/', views.ContactoView.as_view(),name="contacto"),
+    #path('', views.index,name="index"),
+    #path('detalle/<codigo>', views.detalle),
     #path('about/', views.about),
-    path('detalle/<codigo>', views.detalle),
-    path('contacto/', views.contacto),    
+    #path('detalle/<codigo>', views.detalle),
+    #path('contacto/', views.contacto),
 ]
--- eventos/views.py
+++ eventos/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render,get_object_or_404, redirect
+from django.contrib import messages
+from django.urls import reverse_lazy
+
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import FormView
+from django.views import View
 
 from .models import Evento,Persona,Pais
 from datetime import datetime
@@ -15,7 +21,7 @@ class EventosListView(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['titulo'] = "Eventos"
-        context['listado'] = context['evento_list']       
+        context['listado'] = context['evento_list']
 
         return context
 
@@ -29,22 +35,69 @@ def index(request):
     data = {
         'titulo': "Eventos",
         'listado': eventos
-    } 
-    
+    }
+
     return render(request,'index.html',data)
 
-def detalle(request,codigo):
+class detalle(DetailView):
+    model = Evento
+    template_name = 'detalle.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['titulo'] = "Detalle Evento"
+        #print(context)
+        return context
+
+
+def detalle_old(request,codigo):
     #eventos = Evento.objects.get(id=codigo)
     eventos = get_object_or_404(Evento,id=codigo)
 
     data = {
         'titulo': "Detalle",
-        'listado': eventos       
-    } 
+        'listado': eventos
+    }
     return render(request,"detalle.html",data)
 
-def contacto(request):
+
+class contacto(View):
+    template_name = 'contacto.html'
+    def get(self, request):
+        form = ContactoForm()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        form = ContactoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Soporte ingresado, pronto nos comunicaremos")
+            form = ContactoForm()
+            return render(request, self.template_name, {'form': form})
+        else:
+            messages.error(request, "Hubo un error en el formulario")
+            return render(request, self.template_name, {'form': form})
+
+
+from .forms import ContactoForm
+
+class ContactoView(FormView):
+    template_name = "contacto.html"
+    form_class = ContactoForm
+    success_url = reverse_lazy('contacto')  # Reemplace 'contacto' con la URL de éxito que desee
+
+    def form_valid(self, form):
+        form.save()
+        messages.success(self.request, "Soporte ingresado, pronto nos comunicaremos")
+        form = self.form_class()
+        return self.render_to_response(self.get_context_data(form=form))
+
+    def form_invalid(self, form):
+        messages.error(self.request, "Hubo un error en el formulario")
+        return self.render_to_response(self.get_context_data(form=form))
+
+
+def contacto_old(request):
     data = {
         'form': ContactoForm()
     }
